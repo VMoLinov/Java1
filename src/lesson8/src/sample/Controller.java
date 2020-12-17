231413ed1039421b40c364f379e0535b4b27e64e
VMolinov <molinov@list.ru>
2020-12-17 16:25:44 +0300

    Init lesson8

---
+++ src/lesson8/src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+
+    @FXML
+    private TextField numberInputField;
+
+    @FXML
+    private Label header;
+
+    @FXML
+    private Label content;
+
+    @FXML
+    void doRandomAction() {
+
+    }
+
+}
+
